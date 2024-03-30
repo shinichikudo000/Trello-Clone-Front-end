64ffeaa4f920516440dbdae4bcf0106a34fd9ee7
shinichikudo000 <acostayuukichi@gmail.com>
2024-03-30 18:22:45 +0800

    fixs style in a section of landing page

--- src/app/(landing_page)/page.tsx
+++ src/app/(landing_page)/page.tsx
@@ -35,16 +35,10 @@ export default function LandingPage() {
             <section className="">
                 <div className="sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px] mx-auto px-[1rem]">
                     <div className="w-full flex justify-start items-start">
-                        <div>
-                            <div className="flex flex-wrap md:max-w-[60%]">
-                                <p>Trello 101</p>
-                            </div>
-                            <div className="flex flex-wrap md:max-w-[60%]">
-                                <p>A productivity powerhouse</p>
-                            </div>
-                            <div className="flex flex-wrap md:max-w-[60%]">
-                                <p>Simple, flexible, and powerful. All it takes are boards, lists, and cards to get a clear view of who's doing what and what needs to get done. Learn more in our guide for getting started.</p>
-                            </div>
+                        <div className="text-wrap w-full md:max-w-[60%] p-4">
+                                <h3 className="text-base font-medium mb-2">Trello 101</h3>
+                                <h1 className="text-2xl md:text-4xl pb-2 md:pb-4 mb-2">A productivity powerhouse</h1>
+                                <p className="text-xl">Simple, flexible, and powerful. All it takes are boards, lists, and cards to get a clear view of who's doing what and what needs to get done. Learn more in our &nbsp;<Link href={''} className="underline text-blueButton">guide for getting started.</Link></p>
                         </div>
                     </div>
                 </div>
