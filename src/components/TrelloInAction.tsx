cb718f45eb26a1a78ba494d433901de3180aafc1
shinichikudo000 <acostayuukichi@gmail.com>
2024-04-01 15:22:42 +0800

    creates new file for trello in action section

---
+++ src/components/TrelloInAction.tsx
@@ -0,0 +1,17 @@
+export default function TrelloInAction() {
+    return ( 
+        <section>
+            <div className="sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px] mx-auto px-[1rem]">
+                <div className="w-full flex justify-start items-start">
+                    <div className="text-wrap w-full md:max-w-[60%] p-4">
+                            <h3 className="text-base font-medium mb-2">TRELLO IN ACTION</h3>
+                            <h1 className="text-2xl md:text-4xl pb-2 md:pb-4 mb-2">Workflows for any project, big or small</h1>
+                    </div>
+                </div>
+                <div className="w-full">
+
+                </div>
+            </div>
+        </section>
+    )
+}
