2b2176a000e86b97ed8561bdbab340ccdbd82acf
shinichikudo000 <acostayuukichi@gmail.com>
2024-04-05 14:48:37 +0800

    fixs flex style in one section of landing page

--- src/components/WaysToGrow.tsx
+++ src/components/WaysToGrow.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function WaysToGrow() {
     return (
-        <section className="w-full">
+        <section className="w-full mt-8 md:mt-12">
             <div className="sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px] mx-auto px-[1rem]">
                 <div className="w-full text-start md:w-[58%] p-4">
-                    <h3 className="text-base font-medium mb-2">POWERFUL WAYS TO GROW</h3>
+                    <h3 className="text-base font-bold mb-2">POWERFUL WAYS TO GROW</h3>
                     <h1 className="text-2xl md:text-4xl font-bold file:mb-6">Do more with Trello</h1>
                     <p className="text-xl">Trello's intuitive features give any team the ability to quickly set up and customize workflows for just about anything</p>
                 </div>
-                <div className="w-full md:flex flex-wrap justify-around">
+                <div className="w-full md:flex md:flex-wrap md:justify-around md:items-stretch">
                     <div className="p-4 w-full md:w-1/2 lg:w-1/3">
                         <div className="w-full bg-[#FAFBFC] p-6 rounded-md flex flex-col gap-4">
                             <div>
@@ -25,7 +25,7 @@ export default function WaysToGrow() {
                         </div>
                     </div>
                     <div className="p-4 w-full md:w-1/2 lg:w-1/3">
-                        <div className="w-full bg-[#FAFBFC] p-6 rounded-md flex flex-col gap-4">
+                        <div className="w-full bg-[#FAFBFC] p-6 rounded-md flex flex-col gap-4 h-full">
                             <div>
                                 <img src="https://images.ctfassets.net/rz1oowkt5gyp/7wxRW93hvb7858bMsK4LSs/f6fc44fb23dbc6ee9bc6a7f6e2af0fb7/Gears.svg" className="object-containt h-auto"/>
                             </div>
@@ -34,12 +34,12 @@ export default function WaysToGrow() {
                                 <p className="text-base">No-code automation is built into every Trello board. Focus on the work that matters most and let the robots do the rest.</p>
                             </div>
                             <div className="pt-4">
-                                <Link href={''} className="inline-block p-4 border border-[#0065FF] rounded-md text-base hover:bg-[#DEE8FF]">GEt to know Automation</Link>
+                                <Link href={''} className="inline-block p-4 border border-[#0065FF] rounded-md text-base hover:bg-[#DEE8FF]">Get to know Automation</Link>
                             </div>
                         </div>
                     </div>
                     <div className="p-4 w-full md:w-1/2 lg:w-1/3">
-                        <div className="w-full bg-[#FAFBFC] p-6 rounded-md flex flex-col gap-4">
+                        <div className="w-full bg-[#FAFBFC] p-6 rounded-md flex flex-col gap-4 h-full">
                             <div>
                                 <img src="https://images.ctfassets.net/rz1oowkt5gyp/gMfkjoA3yWYG3kat3qjpW/3902bfdfccf08869e33d63bbc9d1969b/Integrations_Puzzle.svg" className="object-containt h-auto"/>
                             </div>
